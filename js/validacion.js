1246c0790962dca7b21f3ab8344d163e92783486
Eva <eva_elizabeth_frola@hotmail.com>
2023-08-09 19:13:43 -0300

    Confirmacion de contraseñas

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,20 @@
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+
+function checkPasswords() {
+
+    let confirmPassword1 = password1.value;
+    let confirmPassword2 = password2.value;
+
+  if (confirmPassword1 === confirmPassword2) {
+    showAlertSuccess()
+  } else {
+    showAlertError()
+  }
+}
+
+password2.addEventListener("input", checkPasswords);
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
