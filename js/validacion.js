ad5cac1f9a4eb6bec5b9de63cbd41ad2a30b9ce7
Administrador <ivan.24.burgio@gmail.com>
2023-08-09 19:00:02 -0300

    funcion de Daniel y de Ivan

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,23 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function validarCheckbox() {
+    var checkbox = document.getElementsByClassName("form-check-input");
+    var estaMarcado = checkbox.checked;
+}
+
+// Espacio de comprobacion de que los input no esten vacios
+function comprobarInputs() {
+}
+const registrarBtn = document.querySelector('#regBtn');
+
+registrarBtn.addEventListener('click', () => {
+    const nombreInput = document.querySelector('#nombre').value.trim();
+    const apellidoInput = document.querySelector('#apellido').value.trim();
+    const mailInput = document.querySelector('#email').value.trim();
+
+    if((nombreInput.lenght === 0) || (apellidoInput.lenght === 0) || (mailInput.lenght === 0)) {
+        showAlertError();
+    }
+});
